14c1ffad0ffcbde4279e914848137c4be665c11a
uteal <wingedh@yandex.ru>
2025-04-15 19:14:19 +0500

    fix

--- helpers/cacheResources.js
+++ helpers/cacheResources.js
@@ -2,7 +2,7 @@
  * A function for preloading graphic assets, all parameters are optional. For it to work, provide
  * either a path to a JSON file with a list of resource paths, or a list of resource paths directly.
  * @param {object} params
- * @param {string} params.fromFile A path to a JSON file with a list of resource paths.
+ * @param {string} params.fromFile A path to a JSON file with a list of resource paths (relative to the file path).
  * @param {string[]} params.fromList An array with resource paths.
  * @param {function} params.onProgress A callback that gets the number of currently loaded and total resources.
  * @param {function} params.onComplete Fires on completion, receiving a boolean indicator of whether the download was error-free.
